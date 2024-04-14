c171b004702e6cedd8b195a2c3c56b6b53c622d6
LuckySiagian <151485564+LuckySiagian@users.noreply.github.com>
2024-04-14 19:48:46 +0700

    Add file Link 2

---
+++ app/config/config.php
@@ -0,0 +1,10 @@
+<?php
+
+    define('BASEURL','http://localhost:8080/PABI/PHPMVC/PHP-MVC-13323037/public');
+
+    //DB
+    define ('DB_HOST','localhost:3308');
+    define ('DB_USER','root');
+    define ('DB_PASS','');
+    define('DB_NAME','phpmvc');
+
---
+++ app/models/User_model.php
@@ -0,0 +1,11 @@
+<?php
+
+    class User_model {
+        private $nama = 'Lewi';
+
+
+        public function getUser()
+        {
+            return $this->nama;
+        }
+    }
